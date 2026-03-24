52c58757b8d4c421809770f9bec7f59d04656989
RGTPES <Maiduyanh7@gmail.com>
2026-03-24 10:27:51 +0700

    Delete src/ss12 directory

--- src/ss12/src/main/java/ex1/Main.java
+++
@@ -1,8 +0,0 @@
-package ex1;
-
-public class Main {
-    public static void main(String[] args) {
-        Service service = new Service();
-        service.login("BS103", "pass789");
-    }
-}
--- src/ss12/src/main/java/ex3/Main.java
+++
@@ -1,7 +0,0 @@
-package ex3;
-
-public class Main {
-    public static void main(String[] args) {
-        new Service().showCost();
-    }
-}
